b58b532392741007ede71682e3fae9e424bd26d4
Jen Tracy <jenniferetracy@gmail.com>
2020-11-16 21:51:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,8 @@ smallest_value =100000
     if value < smallest_value
       smallest_value = value
      end
-     name= name_smallest
+     name = smallest_value
+    
   end
-  puts name_smallest
+  puts smallest_value
 end
